c5268d4ac8ebdda6ca505b3eaae3236e1eb6606b
damontoth <Damon.Toth@hsc.utah.edu>
2024-11-15 11:14:26 -0700

    Added output documentation for pNextGenSize #6

--- R/pNextGenSize.R
+++ R/pNextGenSize.R
@@ -4,10 +4,10 @@
 #' @param y number of total transmissions in generation n+1
 #' @param R mean of negative binomial offspring distribution
 #' @param k dispersion of negative binomial offspring distribution
-#'
+#' @returns The probability of the given number of transmissions
 #' @examples
 #' # With 5 individuals in this generation, what is the probability of
 #' # 0 to 15 transmissions in the next generation?
-#' pNextGenSize(5, 0:15, R=0.2, k=0.1)
+#' pNextGenSize(x=5, y=0:15, R=0.2, k=0.1)
 #' @export
 pNextGenSize <- function(x,y,R,k) exp(lgamma(k*x+y)-lgamma(k*x)-lgamma(y+1)+y*log(R/k)-(k*x+y)*log(1+R/k))
